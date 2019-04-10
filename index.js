aabd7bb97ff6456e03ace0fcaec7d408c371a5c9
山下　鷹徳 <tyamashita@grandream.jp>
2019-04-10 22:15:05 +0900

    ユーザー入力の制御:ボタン、文字逆転

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var app4 = new Vue({
-  el: '#app-4',
+var app5 = new Vue({
+  el: '#app-5',
   data: {
-    todos: [
-      { text: 'Learn JavaScript' },
-      { text: 'Learn Vue' },
-      { text: 'Build something awesome' }
-    ]
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
   }
 })
