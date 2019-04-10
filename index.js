3a2d89c5a874501fcd0eec20cf82f98a2e8aea99
山下　鷹徳 <tyamashita@grandream.jp>
2019-04-10 23:42:21 +0900

    コンポーネントを使用しTODOリストを表示する

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-var app6 = new Vue({
-  el: '#app-6',
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
   data: {
-    message: 'Hello Vue!'
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
   }
 })
