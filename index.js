f103371bc1190016e883ae4dbfc1e0f21518e8da
山下　鷹徳 <tyamashita@grandream.jp>
2019-04-11 09:37:43 +0900

    プロパティの変更を防ぐ

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-Vue.component('todo-item', {
-  props: ['todo'],
-  template: '<li>{{ todo.text }}</li>'
-})
+var obj = {
+  foo: 'bar'
+}
 
-var app7 = new Vue({
-  el: '#app-7',
-  data: {
-    groceryList: [
-      { id: 0, text: 'Vegetables' },
-      { id: 1, text: 'Cheese' },
-      { id: 2, text: 'Whatever else humans are supposed to eat' }
-    ]
-  }
+Object.freeze(obj)
+
+new Vue({
+  el: '#app',
+  data: obj
 })
